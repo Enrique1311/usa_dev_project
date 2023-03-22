6bbcd83566d46aace6768cdd31096f2114b7f53d
Enrique1311 <ejspinelli131176@gmail.com>
2023-03-21 23:54:31 -0300

    Any changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const hamburguerMenu = document.querySelector(".hamburguer-menu");
 const mobileLinks = document.querySelector(".navbar-links");
 const closeMenuIcon = document.querySelector(".close-menu-icon");
+const linkItem = document.querySelector(".link-item");
 
 hamburguerMenu.addEventListener("click", toggleMenu);
 closeMenuIcon.addEventListener("click", toggleMenu);
+linkItem = addEventListener("click", toggleMenu);
 
 function toggleMenu() {
 	mobileLinks.classList.toggle("inactive");
